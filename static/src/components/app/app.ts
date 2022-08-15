a376cc133e4475b23dea2f019a942e2e1ecdd45b
KateBlazhko <kateugnicheva@gmail.com>
2022-08-15 15:53:46 +0300

    init: setup webpack, eslint

---
+++ static/src/components/app/app.ts
@@ -0,0 +1,8 @@
+class App {
+
+  public start() {
+
+  }
+}
+
+export default App
---
+++ static/src/index.ts
@@ -0,0 +1,5 @@
+import "./styles.scss";
+import App from "./components/app/app";
+
+const app: App = new App();
+app.start();
