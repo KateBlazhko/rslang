1236c00d50203889f423d24593db6515f3f9ae01
Aleksander Aleksievich <aleksievisa@gmail.com>
2022-08-18 17:50:44 +0300

    fix: fix btn home 2

--- static/src/components/Header.ts
+++ static/src/components/Header.ts
@@ -56,7 +56,7 @@ class Header {
   addThisActive() {
     const activeElement = this.arrHref.find((item) => item.href === this.location.hash);
     if (activeElement) activeElement.active();
-    else this.obgHeader.home?.active();
+    else this.arrHref[0].active();
   }
 
   render() {
