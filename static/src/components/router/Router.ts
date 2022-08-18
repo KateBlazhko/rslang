6390520b67c53b600da902a1a339c85152385923
KateBlazhko <kateugnicheva@gmail.com>
2022-08-18 14:30:04 +0300

    fix: change routing

--- static/src/components/router/Router.ts
+++ static/src/components/router/Router.ts
@@ -1,44 +1,62 @@
+import Signal from '../common/signal'
+import Control from '../common/control';
+
 class Router {
   private location: Location;
-
-  private container: HTMLElement;
+  private currentPage: Control | null = null
+  private container: Control;
 
   constructor() {
+    this.container = new Control(document.body, 'main', 'page')
+
     this.location = window.location;
-    this.container = document.createElement('main');
+    this.setPage(this.location.hash.slice(1))
     this.hashChange();
   }
 
-  hashChange() {
+  public onGoPage = new Signal<string>();
+
+  private hashChange() {
     window.addEventListener('hashchange', () => {
-      console.log(this.location.hash);
-      switch (this.location.hash.slice(1)) {
-        case 'home':
-          this.container.innerHTML = '<h1>Home</h1>';
-          break;
-        case 'about':
-          this.container.innerHTML = '<h1>About Us</h1>';
-          break;
-        case 'book':
-          this.container.innerHTML = '<h1>Book</h1>';
-          break;
-        case 'sprint':
-          this.container.innerHTML = '<h1>Sprint</h1>';
-          break;
-        case 'audio':
-          this.container.innerHTML = '<h1>Audio</h1>';
-          break;
-        case 'statistics':
-          this.container.innerHTML = '<h1>statistics</h1>';
-          break;
-        default:
-          this.container.innerHTML = '<h1>Home</h1>';
-      }
+      if (this.currentPage) this.currentPage.destroy()
+      const hash = this.location.hash.slice(1)
+      this.setPage(hash)
     });
+    
+  }
+
+  private setPage(hash: string) {
+    const container =  this.container.node
+
+    switch (hash) {
+      case 'home':
+        this.onGoPage.emit(hash)
+        container.innerHTML = '<h1>Home</h1>';
+        break;
+      case 'about':
+        container.innerHTML = '<h1>About Us</h1>';
+        break;
+      case 'book':
+        this.onGoPage.emit(hash)
+        container.innerHTML = '<h1>Book</h1>';
+        break;
+      case 'sprint':
+        container.innerHTML = '';
+        // this.currentPage = new Sprint(container, this.onGoPage)  
+        break;
+      case 'audio':
+        container.innerHTML = '<h1>Audio</h1>';
+        break;
+      case 'statistics':
+        container.innerHTML = '<h1>statistics</h1>';
+        break;
+      default:
+        container.innerHTML = '<h1>Home</h1>';
+    }
   }
 
   render() {
-    return this.container;
+    return this.container.node;
   }
 }
 
