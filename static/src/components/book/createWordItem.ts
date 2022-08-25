2fa8900c989786164d4286c5dade492cb44930f2
Yury Kalenkou <goldyukol12@gmail.com>
2022-08-25 16:58:42 +0200

    fix: create word item function

--- static/src/components/book/createWordItem.ts
+++ static/src/components/book/createWordItem.ts
@@ -4,44 +4,6 @@ import Control from "../common/control";
 import stopPlayAudio from "../common/stopPlayAudio";
 
 const createWordItem = (word: IWord, bookItem: Control<HTMLElement>) => {
-  //   const cardWrapper = new CustomElement("div", {
-  //     className: "section__cards-card card",
-  //   });
-
-  //   const cardImage = new CustomElement("img", {
-  //     className: "card__image",
-  //     src: `${config.API.URL}/${word.image}`,
-  //     alt: word.word,
-  //   });
-
-  //   const cardInfo = new CustomElement("div", {
-  //     className: "card__info",
-  //   });
-
-  //   const resources = [
-  //     word.word,
-  //     word.transcription,
-  //     word.wordTranslate,
-  //     word.textMeaning,
-  //     word.textMeaningTranslate,
-  //     word.textExample,
-  //     word.textExampleTranslate,
-  //   ];
-
-  //   resources.forEach((res) => {
-  //     const fieldElement = new CustomElement("p", {
-  //       className: "card__info-field",
-  //       innerHTML: res,
-  //     });
-  //     cardInfo.addChildren([fieldElement.element]);
-  //   });
-
-  // const soundElement = new CustomElement("img", {
-  //   className: "card__sound",
-  //   src: soundIcon,
-  //   alt: "sound-icon",
-  // });
-
   const imageElement = new Control(bookItem.node, "img", "book__item-image");
   const image = imageElement.node as HTMLImageElement;
   image.src = `${API_URL}/${word.image}`;
