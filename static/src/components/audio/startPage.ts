7a8338b833a801b74b774bb85bcb15b69373c399
Aleksander Aleksievich <aleksievisa@gmail.com>
2022-08-28 17:32:01 +0300

    feat: reafacit style page game audio

--- static/src/components/audio/startPage.ts
+++ static/src/components/audio/startPage.ts
@@ -1,8 +1,6 @@
 import Control from '../common/control';
 
 class StartPageAudio extends Control {
-  private title: Control<HTMLElement>;
-
   private arrBtn: Array<{node: HTMLButtonElement, difficult: number}>;
 
   private containerBtn: Control<HTMLElement>;
@@ -11,16 +9,39 @@ class StartPageAudio extends Control {
 
   public startBtn: Control<HTMLElement>;
 
+  description: Control<HTMLElement>;
+
   constructor(
     parentNode: HTMLElement | null,
   ) {
     super(parentNode, 'div', 'start__page__audio');
-    this.title = new Control(this.node, 'h1', 'audio_call__title', 'Выберите сложность и нажмите начать');
+    this.description = new Control(this.node, 'div', 'description__audio');
+    this.createDescription();
     this.containerBtn = new Control(this.node, 'div', 'audio_container__btn');
     this.startBtn = new Control(this.node, 'button', 'start_btn__audio', 'START');
     this.arrBtn = [];
     this.difficult = 1;
     this.createBtnDifficult();
+  }
+
+  createDescription() {
+    this.description.node.innerHTML = `
+      <div class='preview'>
+        <h2>Audio Call</h2>
+        <p>Captain! You need to guess the word by ear!!!</p>
+      </div>
+      <div class='instruction'>
+        <p>You can use a mouse or keyboard</p>
+      </div>
+      <div class='key-instruction'>
+        <p>To select a word, press:</p>
+        <span>1</span><span>2</span><span>3</span><span>4</span><span>5</span><span>6</span>
+      </div>
+      <div class='key-instruction-two'>
+        <p>To get the next card, press:</p>
+        <div class='key'><span>Enter</span></div>
+      </div>
+    `;
   }
 
   private createBtnDifficult(): void {
