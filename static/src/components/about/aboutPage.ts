7f558fe4ff1ef4d979c771328cf41c11bebf3cbc
Yury Kalenkou <goldyukol12@gmail.com>
2022-08-30 12:54:24 +0200

    feat: add draw team logic

--- static/src/components/about/aboutPage.ts
+++ static/src/components/about/aboutPage.ts
@@ -1,31 +1,83 @@
+import ButtonHref from "../common/ButtonHref";
 import Control from "../common/control";
+import TEAM_DATA from "./teamData";
 
 enum TextInner {
   title = "Our team",
   introductoryText = "We are Team #26 of the Rolling Scopes School JavaScript/Front-end course, providing you with an app to learn English!",
 }
 
+interface ITeamMember {
+  name: string;
+  link: string;
+  position: string;
+  avatar: string;
+}
+
 class AboutPage extends Control {
-  // private teamWrap: Control;
+  private teamWrap: Control;
 
   constructor(private parentNode: HTMLElement | null) {
     super(parentNode, "div", "about");
-    const titleWrap = new Control(this.node, "div", "about__block-wrap");
-    new Control(titleWrap.node, "h2", "about__title", TextInner.title);
+    const titleTeamWrap = new Control(this.node, "div", "about__block-wrap");
+    new Control(titleTeamWrap.node, "h2", "about__title", TextInner.title);
     new Control(
-      titleWrap.node,
+      titleTeamWrap.node,
       "span",
       "about__text",
       TextInner.introductoryText
     );
-    // this.teamWrap = new Control(this.node, "div", "about__team");
+
+    this.teamWrap = new Control(this.node, "div", "about__team");
 
     this.initPage();
   }
 
-  private initPage() {}
+  private initPage() {
+    this.drawTeam();
+  }
 
-  private drawTeam() {}
+  private drawTeam() {
+    TEAM_DATA.forEach((teamMemberItem: ITeamMember) =>
+      this.drawTeamMember(teamMemberItem)
+    );
+  }
+
+  private drawTeamMember(teamMemberItem: ITeamMember) {
+    const teamMemberWrap = new Control(
+      this.teamWrap.node,
+      "div",
+      "about__team-member"
+    );
+
+    const avatarWrap = new ButtonHref(
+      teamMemberWrap.node,
+      `${teamMemberItem.link}`,
+      "",
+      `about__avatar-wrap`
+    );
+    avatarWrap.node.setAttribute("target", "_blank");
+
+    const avatarImage = new Control<HTMLImageElement>(
+      avatarWrap.node,
+      "img",
+      "about__avatar-img"
+    );
+    avatarImage.node.src = teamMemberItem.avatar;
+
+    new Control(
+      teamMemberWrap.node,
+      "p",
+      "about__team-member-name",
+      teamMemberItem.name
+    );
+    new Control(
+      teamMemberWrap.node,
+      "p",
+      "about__team-member-name",
+      teamMemberItem.position
+    );
+  }
 }
 
 export default AboutPage;
---
+++ static/src/components/about/teamData.ts
@@ -0,0 +1,28 @@
+const TEAM_DATA = [
+  {
+    name: "Nikita",
+    link: "https://github.com/kubarenatioN",
+    position: "Mentor",
+    avatar: "https://avatars.githubusercontent.com/u/65170141?v=4",
+  },
+  {
+    name: "Kate",
+    link: "https://github.com/KateBlazhko",
+    position: "Team Lead",
+    avatar: "https://avatars.githubusercontent.com/u/92632914?v=4",
+  },
+  {
+    name: "Aleksander",
+    link: "https://github.com/Alex99like",
+    position: "Developer",
+    avatar: "https://avatars.githubusercontent.com/u/76603765?v=4",
+  },
+  {
+    name: "Yury",
+    link: "https://github.com/goldyukol",
+    position: "Developer",
+    avatar: "https://avatars.githubusercontent.com/u/50366889?v=4",
+  },
+];
+
+export default TEAM_DATA;
