67988efb93010b7bc2455f9614c4aa94908eb099
Yury Kalenkou <goldyukol12@gmail.com>
2022-08-30 13:27:39 +0200

    feat: add local avatars

--- static/src/components/about/teamData.ts
+++ static/src/components/about/teamData.ts
@@ -3,25 +3,25 @@ const TEAM_DATA = [
     name: "Nikita",
     link: "https://github.com/kubarenatioN",
     position: "Mentor",
-    avatar: "https://avatars.githubusercontent.com/u/65170141?v=4",
+    avatar: "./assets/img/nikita.jpeg",
   },
   {
     name: "Kate",
     link: "https://github.com/KateBlazhko",
     position: "Team Lead",
-    avatar: "https://avatars.githubusercontent.com/u/92632914?v=4",
+    avatar: "./assets/img/kate.jpeg",
   },
   {
     name: "Aleksander",
     link: "https://github.com/Alex99like",
     position: "Developer",
-    avatar: "https://avatars.githubusercontent.com/u/76603765?v=4",
+    avatar: "./assets/img/alex.jpeg",
   },
   {
     name: "Yury",
     link: "https://github.com/goldyukol",
     position: "Developer",
-    avatar: "https://avatars.githubusercontent.com/u/50366889?v=4",
+    avatar: "./assets/img/yury.jpeg",
   },
 ];
 
